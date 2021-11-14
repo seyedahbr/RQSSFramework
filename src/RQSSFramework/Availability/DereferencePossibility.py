00b73c986832a945c6cc2627f3a890a99ef7944c
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2021-11-14 11:33:09 +0000

    Added the Availability_dim dimension (deref_metric)

---
+++ src/RQSSFramework/Availability/DereferencePossibility.py
@@ -0,0 +1,38 @@
+import requests
+from typing import Tuple, List, Iterator, NamedTuple
+from rdflib import URIRef
+
+class DerefOfURI(NamedTuple):
+    uri: URIRef
+    deref: bool
+
+class DerefrenceExplorer:
+    _uris=[]
+    results=None
+    def __init__(self, uris: Iterator[URIRef]):
+        self._uris = list(uris)
+    
+    def check_dereferencies(self) -> List[DerefOfURI]:
+        """
+        check the accssessibility of each URI in the class
+        """
+        self.results=[]
+        for u in self._uris:
+            try:
+                r = requests.get(u)
+                if r.status_code == 200:
+                    self.results.append(DerefOfURI(u,True))
+                else:
+                    self.results.append(DerefOfURI(u,False))
+            except Exception as e:
+                print(e)
+                
+    def print_results(self):
+        """
+        print self.results if it is already computed
+        """
+        if self.results == None:
+            print('Results are not computed')
+            return
+        for r in self.results:
+            print("URI:{0}, Deref:{1}".format(r.uri,r.deref))
