f78b9cd324700e04a777d47cb30ef632002a126e
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2021-11-22 21:17:11 +0000

    remove debug print

--- src/RQSSFramework/Licensing/LicenseExistanceChecking.py
+++ src/RQSSFramework/Licensing/LicenseExistanceChecking.py
@@ -90,7 +90,6 @@ class LicenseChecker:
         for u in self._domains:
             try:
                 r = requests.get(u, verify=False)
-                print(r.status_code)
                 if (r.status_code == 200 and self.html_contains_license(r.text.lower())):   # main human readable license existance checking
                     self.results.append(LicExistOfDom(u,True))
                 else:
