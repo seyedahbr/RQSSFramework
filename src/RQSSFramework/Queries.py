226cda3b73206cbfd38a75efb0295c398feae75e
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2021-12-03 17:09:29 +0000

    modified external source extraction queries

--- src/RQSSFramework/Queries.py
+++ src/RQSSFramework/Queries.py
@@ -1,42 +1,55 @@
 RQSS_QUERIES={
+"test_query":
+'''
+SELECT ?to_ret WHERE
+{
+  ?statement prov:wasDerivedFrom ?ref.
+  ?ref pr:P854 str(?to_ret).
+  FILTER(isIRI(str(?to_ret))).
+}
+
+Limit 3
+''',
+
+
 "get_all_external_sources_filter_wikimedia" : 
 '''
 PREFIX prov: <http://www.w3.org/ns/prov#>
-SELECT ?refValue WHERE {
+SELECT ?to_ret WHERE {
    ?statement prov:wasDerivedFrom ?ref .
-   ?ref ?refProperty ?refValue .
-  FILTER (CONTAINS(lcase(?refValue), "http") &&
-          !CONTAINS(lcase(?refValue), "wikidata.org") &&
-          !CONTAINS(lcase(?refValue), "wikipedia.org") &&
-          !CONTAINS(lcase(?refValue), "wikimedia.org") &&
-          !CONTAINS(lcase(?refValue), "wikivoyage.org")&&
-          !CONTAINS(lcase(?refValue), "mediawiki.org") &&
-          !CONTAINS(lcase(?refValue), "wikiversity.org")&&
-          !CONTAINS(lcase(?refValue), "wikinews.org")  &&
-          !CONTAINS(lcase(?refValue), "wikisource.org")&&
-          !CONTAINS(lcase(?refValue), "wikibooks.org") &&
-          !CONTAINS(lcase(?refValue), "wikiquote.org") &&
-          !CONTAINS(lcase(?refValue), "wiktionary.org"))
+   ?ref ?refProperty ?to_ret .
+  FILTER (isIRI(?to_ret) &&
+          !CONTAINS(lcase(str(?to_ret)), "wikidata.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikipedia.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikimedia.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikivoyage.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "mediawiki.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikiversity.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "wikinews.org")  &&
+          !CONTAINS(lcase(str(?to_ret)), "wikisource.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "wikibooks.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikiquote.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wiktionary.org"))
 }
 ''',
 "get_all_external_sources_filter_wikimedia_distinct" : 
 '''
 PREFIX prov: <http://www.w3.org/ns/prov#>
-SELECT DISTINCT ?refValue WHERE {
+SELECT DISTINCT ?to_ret WHERE {
    ?statement prov:wasDerivedFrom ?ref .
-   ?ref ?refProperty ?refValue .
-  FILTER (CONTAINS(lcase(?refValue), "http") &&
-          !CONTAINS(lcase(?refValue), "wikidata.org") &&
-          !CONTAINS(lcase(?refValue), "wikipedia.org") &&
-          !CONTAINS(lcase(?refValue), "wikimedia.org") &&
-          !CONTAINS(lcase(?refValue), "wikivoyage.org")&&
-          !CONTAINS(lcase(?refValue), "mediawiki.org") &&
-          !CONTAINS(lcase(?refValue), "wikiversity.org")&&
-          !CONTAINS(lcase(?refValue), "wikinews.org")  &&
-          !CONTAINS(lcase(?refValue), "wikisource.org")&&
-          !CONTAINS(lcase(?refValue), "wikibooks.org") &&
-          !CONTAINS(lcase(?refValue), "wikiquote.org") &&
-          !CONTAINS(lcase(?refValue), "wiktionary.org"))
+   ?ref ?refProperty ?to_ret .
+  FILTER (isIRI(?to_ret) &&
+          !CONTAINS(lcase(str(?to_ret)), "wikidata.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikipedia.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikimedia.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikivoyage.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "mediawiki.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikiversity.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "wikinews.org")  &&
+          !CONTAINS(lcase(str(?to_ret)), "wikisource.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "wikibooks.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikiquote.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wiktionary.org"))
 }
 '''
 }
