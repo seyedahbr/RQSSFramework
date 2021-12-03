80e89a9a63036388f125eb1c983b8bd37732ead4
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2021-12-03 22:06:15 +0000

    minor corrections

--- src/RQSSFramework/RQSS-Extractor.py
+++ src/RQSSFramework/RQSS-Extractor.py
@@ -48,7 +48,7 @@ def extract_from_file(opts: ArgumentParser) -> int:
 def extract_from_endpoint(opts: ArgumentParser) -> int:
 
     if(opts.extract_external):
-        external_uris=perform_query(opts.endpoint, RQSS_QUERIES["test_query"])#RQSS_QUERIES["get_all_external_sources_filter_wikimedia"])
+        external_uris=perform_query(opts.endpoint, RQSS_QUERIES["get_all_external_sources_filter_wikimedia"])
         output_file = os.path.join(opts.output_dir + os.sep + 'external_uris.data')
         with open(output_file, 'w') as file_handler:
             for uri in external_uris:
