bffb25b767b245ae4b38b655f962d9c3870c2a8d
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2021-12-03 22:25:36 +0000

    minor corrections

--- src/RQSSFramework/RQSS-Extractor.py
+++ src/RQSSFramework/RQSS-Extractor.py
@@ -1,4 +1,3 @@
-from base64 import encodestring
 import sys
 import os
 from datetime import datetime
@@ -50,7 +49,7 @@ def extract_from_endpoint(opts: ArgumentParser) -> int:
 
     if(opts.extract_external):
         start_time=datetime.now()
-        
+
         external_uris=perform_query(opts.endpoint, RQSS_QUERIES["get_all_external_sources_filter_wikimedia"])
         output_file = os.path.join(opts.output_dir + os.sep + 'external_uris.data')
         with open(output_file, 'w') as file_handler:
