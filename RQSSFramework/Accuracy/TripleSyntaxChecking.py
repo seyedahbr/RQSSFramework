e5201d06ab12a4cfa885040f104714149f932259
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2021-12-16 23:54:22 +0000

    minor additions

--- RQSSFramework/Accuracy/TripleSyntaxChecking.py
+++ RQSSFramework/Accuracy/TripleSyntaxChecking.py
@@ -1,6 +1,16 @@
 from typing import Iterator, List, NamedTuple
 
 class WikibaseSyntaxChecker:
-    
+    _num_fails = None
+
     def __init__(self):
-        return
+        self._num_fails = 0
+    
+    def print_results(self):
+        """
+        print self._num_fains if it is already computed
+        """
+        if self._num_fails == None:
+            print('Results are not computed')
+            return
+        print("Number of syntax fails:{1}".format(self._num_fails))
