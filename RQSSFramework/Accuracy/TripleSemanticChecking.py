919eff160cf2990fbb193a90f3e0af44c54c7af1
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-01-24 23:59:27 +0000

    modified semantic checking

--- RQSSFramework/Accuracy/TripleSemanticChecking.py
+++ RQSSFramework/Accuracy/TripleSemanticChecking.py
@@ -1,44 +1,75 @@
-from re import S
 from typing import List, NamedTuple, Dict
 
+from sqlalchemy import true
+
+
 class TripleSemanticResult(NamedTuple):
-    property: str
-    total: int
-    matches: int
+    property: int       # the property at the heart of the fact
+    half_matches: int   # number of subject-relation matches in reference triples
+    full_matches: int   # number of exact matches in reference triples
+
     def __repr__(self):
-        return "Number of checked values for propety {0}: {1}; exact matches:{2}; score:{3}".format(self.property, self.total, self.matches, self.score)
+        return "For propety {0}: number of subject_relation matches: {1}; exact matches:{2}; score:{3}".format(self.property, self.half_matches, self.full_matches, self.score)
+
     @property
     def score(self):
-        return (self.matches/self.total)
+        return (self.full_matches/self.half_matches) if self.half_matches != 0 else 1.0
 
-class RefPropValue(NamedTuple):
+
+class FactReference(NamedTuple):
+    subject: str
+    property: str
     ref_property: str
     ref_value: str
 
+
 class RefTripleSemanticChecker:
     results: List[TripleSemanticResult] = None
-    _gold_standard: Dict
-    _statement_ref_value: Dict
-    
-    def __init__(self,gold_standard: Dict, statement_ref_value: Dict):
+    _gold_standard: List[FactReference]
+    _fact_ref_triples: List[FactReference]
+
+    def __init__(self, gold_standard: List[FactReference], fact_ref_triples: List[FactReference]):
         self._gold_standard = gold_standard
-        self._statement_ref_value = statement_ref_value
-    
+        self._fact_ref_triples = fact_ref_triples
+
     def check_semantic_to_gold_standard(self) -> List[TripleSemanticResult]:
         self.results = []
-        for prop in self._statement_ref_value.keys():
-            num_matches = 0
-            num_fails = 0
-            
+        prop_results = {}
+        for fact_gs in self._gold_standard:
+            if fact_gs.property not in prop_results.keys():
+                prop_results[str(fact_gs.property)] = [0, 0]
+            for ref_triple in self._fact_ref_triples:
+                if self.is_sub_rel_match(ref_triple, fact_gs):
+                    prop_results[str(fact_gs.property)][0] += 1
+                    if self.is_triple_exact_match(ref_triple, fact_gs):
+                        prop_results[str(fact_gs.property)][1] += 1
+        for prop in prop_results.keys():
+            self.results.append(TripleSemanticResult(str(prop), prop_results[str(prop)][0], prop_results[str(prop)][1]))
+        return self.results
 
+    def is_sub_rel_match(self, ref_triple: FactReference, fact_gs: FactReference) -> bool:
+        if ref_triple.subject == fact_gs.subject and\
+           ref_triple.property == fact_gs.property and\
+           ref_triple.ref_property == fact_gs.ref_property:
+            return true
+        # other equivalency conditions can be placed here
+        # for example doing :sameAs searchs
 
+    def is_triple_exact_match(self, ref_triple: FactReference, fact_gs: FactReference) -> bool:
+        if ref_triple.subject == fact_gs.subject and\
+           ref_triple.property == fact_gs.property and\
+           ref_triple.ref_property == fact_gs.ref_property and\
+           ref_triple.ref_value == fact_gs.ref_value:
+            return true
+        # other equivalency conditions can be placed here
+        # for example doing :sameAs searchs
 
     def print_results(self):
         """
         print self.result if it is already computed
         """
-        if self.result == None:
+        if self.results == None:
             print('Results are not computed')
             return
         for result in self.results:
-            print(self.result)
+            print(result)
--- tests/test_TestTripleSemanticChecker.py
+++ tests/test_TestTripleSemanticChecker.py
@@ -1,17 +1,38 @@
 import unittest
-from RQSSFramework.Accuracy.TripleSemanticChecking import RefTripleSemanticChecker,RefPropValue,TripleSemanticResult
+from RQSSFramework.Accuracy.TripleSemanticChecking import RefTripleSemanticChecker, FactReference, TripleSemanticResult
+
 
 class TestRefTripleSemanticChecking(unittest.TestCase):
 
     def setUp(self):
-        self.GSdata = {'P21':[RefPropValue('P248','Q36578'),RefPropValue('P248','Q77541206')],
-        'P27':[RefPropValue('P3865','Q83900')],
-        'P1412':[RefPropValue('P248','Q19938912')]}
-        self.data = {'P21':[(),(),()]}
+        self.GSdata = [FactReference('Q1', 'P21', 'P248', 'Q36578'), FactReference('Q1', 'P21', 'P248', 'Q77541206'),
+                       FactReference('Q1', 'P27', 'P3865', 'Q83900'), FactReference('Q1', 'P1412', 'P248', 'Q19938912')]
+        self.data =[
+            FactReference('Q1','P21','P248', 'Q77541206'), FactReference('Q1','P21','P248', 'Q36578'), FactReference('Q1','P21','P248', 'Q1'),
+            FactReference('Q1','P21','P248', 'Q2'), FactReference('Q1','P21','P248', 'Q3'), FactReference('Q1','P21','P1', 'Q2'), FactReference('Q1','P21','P2', 'Q3'),
+            FactReference('Q1','P27','P1', 'Q2'), FactReference('Q1','P27','P2', 'Q3'), FactReference('Q1','P27','P3', 'Q4'),
+            FactReference('Q1','P1412','P248', 'Q2'), FactReference('Q1','P1412','P248', 'Q3'), FactReference('Q1','P1412','P248', 'Q4')]
 
     def test_check_semantic_to_gold_standard(self):
-        ret = RefTripleSemanticChecker(self.GSdata, self.data).check_semantic_to_gold_standard()
-        self.assertEqual(ret.keys(), self.data.keys())
+        ret = RefTripleSemanticChecker(self.GSdata, self.data)
+        ret.check_semantic_to_gold_standard()
+        for result in ret.results:
+            # note that in P21 example, there are two similar specified ref triples in the gold standard and 5 exact matches in data 
+            # but we count every 5 in data twice (for each gold standard triple)
+            # The higer non-exact-match ref values, the lower accuracy we will have
+            # The higer specified values in gold standard, the higher impact and expectation of exact we will have
+            if result.property == 'P21':
+                self.assertEqual(result.half_matches,10)
+                self.assertEqual(result.full_matches,2)
+                self.assertEqual(result.score,0.2)
+            if result.property == 'P27':
+                self.assertEqual(result.half_matches,0)
+                self.assertEqual(result.full_matches,0)
+                self.assertEqual(result.score,1.0)
+            if result.property == 'P1412':
+                self.assertEqual(result.half_matches,3)
+                self.assertEqual(result.full_matches,0)
+                self.assertEqual(result.score,0)
+
 
         
-
