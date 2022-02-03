1e1c639e927a45254b63d67e790703201baa7451
Seyed Amir Hosseini <a.beghaei@gmail.com>
2022-02-03 08:28:11 +0000

    added user agent to SPARQL wrapper

--- RQSSFramework/Consistency/RefPropertiesConsistencyChecking.py
+++ RQSSFramework/Consistency/RefPropertiesConsistencyChecking.py
@@ -1,31 +1,38 @@
+import sys
 from typing import List, NamedTuple
-from SPARQLWrapper import JSON, SPARQLWrapper
 
 from Queries import RQSS_QUERIES
+from SPARQLWrapper import JSON, SPARQLWrapper
 
 
 class PropConsistencyResult(NamedTuple):
     property: str
     is_ref_specific: bool
+
     def __repr__(self):
         return "Property {0} is reference-specific: {1}".format(self.property, self.is_ref_specific)
+
 
 class RefPropertiesConsistencyChecker:
     results: List[PropConsistencyResult] = None
     _ref_properties: List[str]
 
-    def __init__(self, ref_properties:List[str]):
+    def __init__(self, ref_properties: List[str]):
         self._ref_properties = ref_properties
-    
+
     def check_reference_specificity_from_Wikdiata(self) -> List[PropConsistencyResult]:
         self.results = []
-        sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+        user_agent = "RQSSFramework Python/%s.%s" % (
+            sys.version_info[0], sys.version_info[1])
+        sparql = SPARQLWrapper(
+            "https://query.wikidata.org/sparql", agent=user_agent)
         for prop in self._ref_properties:
-            sparql.setQuery(RQSS_QUERIES['get_property_constraints_specificity'].format(prop))
+            sparql.setQuery(
+                RQSS_QUERIES['get_property_constraints_specificity'].format(prop))
             sparql.setReturnFormat(JSON)
             results = sparql.query().convert()
             for result in results["results"]["bindings"]:
-                value=result["to_ret"]["value"]
+                value = result["to_ret"]["value"]
                 if value == 'true':
                     self.results.append(PropConsistencyResult(prop, True))
                 else:
