856130923c2081595aef3bd71d2880eff2fbdd32
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-02-09 23:19:55 +0000

    added reputation (blacklisted)

---
+++ RQSSFramework/Reputation/DNSBLBlacklistedChecking.py
@@ -0,0 +1,47 @@
+from typing import Iterator, List, NamedTuple
+from urllib.parse import urlparse
+
+import pydnsbl
+from rdflib import URIRef
+
+
+class BlacklistedOfDom(NamedTuple):
+    domain: URIRef
+    blacklisted: bool
+    def __repr__(self):
+        return "Domain:{0:40}, is in blacklist:{1}".format(self.domain, self.blacklisted)
+
+class DNSBLBlacklistedChecker:
+    _domains=[]
+    results=None
+    def __init__(self, uris: Iterator[URIRef]):
+        _uris = list(dict.fromkeys(uris)) # remove duplicated URIs
+        self._domains = dict.fromkeys(self.domain_extractor(_uris)) # compute and remove duplicated domains
+    
+    def domain_extractor(self,uris: Iterator[URIRef]) -> List[URIRef]:
+        ret_list=[]
+        for u in uris:
+            ret_list.append('https://'+urlparse(u).netloc)
+        return ret_list
+
+    def check_domain_blacklisted(self) -> List[BlacklistedOfDom]:
+        self.results=[]
+        domain_checker = pydnsbl.DNSBLDomainChecker()
+        for u in self._domains:
+            try:
+                r = domain_checker.check(u)
+                self.results.append(BlacklistedOfDom(u,r.blacklisted))
+            except Exception as e:
+                self.results.append(BlacklistedOfDom(u,False))
+                print(e)
+        return self.results
+                
+    def print_results(self):
+        """
+        print self.results if it is already computed
+        """
+        if self.results == None:
+            print('Results are not computed')
+            return
+        for r in self.results:
+            print(r)
