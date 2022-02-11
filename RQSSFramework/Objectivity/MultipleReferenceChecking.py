001ae47a32532d7c73f207a3a6c4d5c5d4528b88
Seyed Amir Hosseini <a.beghaei@gmail.com>
2022-02-11 11:50:56 +0000

    added multiple references metric

---
+++ RQSSFramework/Objectivity/MultipleReferenceChecking.py
@@ -0,0 +1,43 @@
+from typing import List, NamedTuple
+
+
+class MultipleRefResult(NamedTuple):
+    total_statement: int
+    multiple_referenced: int
+
+    def __repr__(self):
+        return "Number of statements: {0}; statements with multiple references:{1}; ratio: {2}".format(self.total_statement, self.multiple_referenced, round(self.ratio, 2))
+
+    @property
+    def ratio(self):
+        return (self.multiple_referenced/self.total_statement)
+
+
+class StatementRefNum(NamedTuple):
+    statement: str
+    num_of_refs: int
+
+
+class MultipleReferenceChecker:
+    result: MultipleRefResult = None
+    statements: List[StatementRefNum]
+
+    def __init__(self, statements: List[StatementRefNum]) -> None:
+        self.statements = statements
+
+    def count_seperate_multiple_referenced_statements(self) -> List[StatementRefNum]:
+        ret_val: List[StatementRefNum] = []
+        for record in self.statements:
+            if int(record.num_of_refs) > 1:
+                ret_val.append(record)
+        self.result = MultipleRefResult(len(self.statements), len(ret_val))
+        return ret_val
+
+    def print_results(self):
+        """
+        print self.result if it is already computed
+        """
+        if self.result == None:
+            print('Results are not computed')
+            return
+        print(self.result)
