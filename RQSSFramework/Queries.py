c8c88a28d8a207abd3fbc0361c177cace7616adb
Seyed Amir Hosseini <a.beghaei@gmail.com>
2022-02-11 12:20:27 +0000

    modified ref sharing query removed unnecessary select

--- RQSSFramework/Queries.py
+++ RQSSFramework/Queries.py
@@ -16,12 +16,10 @@ PREFIX wikibase: <http://wikiba.se/ontology#>
 PREFIX prov: <http://www.w3.org/ns/prov#>
 PREFIX wdref: <http://www.wikidata.org/reference/>
 
-SELECT ?refnode (?numOfStatements AS ?num) WHERE{
 SELECT  DISTINCT ?refnode (COUNT(?item) AS ?numOfStatements) WHERE{
         ?item a wikibase:Statement.
         ?item prov:wasDerivedFrom ?refnode.
 }GROUP BY ?refnode
-}
 ''',
 "get_ref_properties_object_value_types_wikimedia":
 '''
