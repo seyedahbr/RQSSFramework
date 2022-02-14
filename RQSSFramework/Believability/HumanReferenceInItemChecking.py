05b2f3eae21d0fefc8f835bde2e90e9de65285a2
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-02-14 22:29:46 +0000

    added human reference code based on xpath from Wikipedia history pages of each item

--- RQSSFramework/Believability/HumanReferenceInItemChecking.py
+++ RQSSFramework/Believability/HumanReferenceInItemChecking.py
@@ -1,4 +1,6 @@
 from typing import Dict, List, NamedTuple
+import requests
+from lxml import html
 
 
 class HumanAddedResult(NamedTuple):
@@ -22,6 +24,20 @@ class HumanReferenceInItemChecker:
         self._item_refed_facts = item_referenced_facts
 
     def check_referenced_facts_human_added(self) -> List[HumanAddedResult]:
+        self.results = []
+        for item in self._item_refed_facts.keys():
+            num_human_added = 0
+            history_page = requests.get('https://www.wikidata.org/w/index.php?title={}&offset=&limit=5000&action=history'.format(str(item)))
+            tree = html.fromstring(history_page.content)
+            for prop in self._item_refed_facts[str(item)]:
+                revisions_adder = tree.xpath('//*[@id="pagehistory"]/li[./span/span/span/text()="Added reference to claim: " and ./span/a/span/span/text()="({})"]/span[@class="history-user"]/a/bdi/text()'.format(prop))
+                revisions_chanr = tree.xpath('//*[@id="pagehistory"]/li[./span/span/span/text()="Changed reference of claim: " and ./span/a/span/span/text()="({})"]/span[@class="history-user"]/a/bdi/text()'.format(prop))
+                revisions_adder.extend(revisions_chanr)
+                for editor in revisions_adder:
+                    if 'bot' not in editor.lower():
+                        num_human_added += 1
+                        break
+            self.results.append(HumanAddedResult(str(item),len(self._item_refed_facts[str(item)]),num_human_added))
         return self.results
 
     def print_results(self):
@@ -33,3 +49,14 @@ class HumanReferenceInItemChecker:
             return
         for result in self.results:
             print(result)
+
+    @property
+    def score(self):
+        return sum([x.score for x in self.results])/len(self.results)
+    
+    def __repr__(self):
+        if self.results == None:
+            return 'Results are not computed'            
+        return """num of items, num of referenced facts, score
+        {0},{1},{2}
+        """.format(len(self.results),sum([x.total for x in self.results]),self.score)
