5291c14833203fa614c1d3e3d75272961fd6e478
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-02-15 11:12:23 +0000

    added test for human added references

---
+++ tests/test_TestHumanReferenceInItemChecker.py
@@ -0,0 +1,29 @@
+import unittest
+import csv
+from RQSSFramework.Believability.HumanReferenceInItemChecking import *
+
+class TestHumanReferenceInItemChecking(unittest.TestCase):
+
+    def setUp(self):
+        self.data = {'Q4822345':['P279','P1402','P1343','P1403','P2581','P3720'],'Q2':['P910','P571', 'P610', 'P1589', 'P1082', 'P2067']}
+
+    def test_check_referenced_facts_human_added(self):
+        test_class = HumanReferenceInItemChecker(self.data)
+        self.assertEqual(test_class.results,None)
+        ret = test_class.check_referenced_facts_human_added()
+        self.assertEqual(len(ret), len(self.data.keys()))
+        self.assertGreaterEqual(test_class.score,0)
+        self.assertLessEqual(test_class.score,1)
+        with open('human_added_ratio.test.csv', 'w') as file_handler:
+            file_handler.write(str(test_class))
+        with open('human_added.test.csv', 'w', newline='') as f:
+            w = csv.writer(f)
+            # write header from NamedTuple fields
+            w.writerow([field for field in ret[0]._fields])
+            for result in ret:
+                row = [result._asdict()[field] for field in result._fields]
+                w.writerow(row)
+        
+
+        
+
