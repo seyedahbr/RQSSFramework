0c58c39cad6bcc70f298f68de563950e2bb1a961
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-02-24 12:19:48 +0000

    modified test examining only know data not unpredictable cases

--- tests/test_TestPropertyConsistencyChecker.py
+++ tests/test_TestPropertyConsistencyChecker.py
@@ -1,19 +1,26 @@
+import csv
 import unittest
-from RQSSFramework.Consistency.RefPropertiesConsistencyChecking import RefPropertiesConsistencyChecker
+
+from RQSSFramework.Consistency.RefPropertiesConsistencyChecking import \
+    RefPropertiesConsistencyChecker
+
 
 class TestPropertyConsistencyChecking(unittest.TestCase):
 
     def setUp(self):
-        self.data = ['P854','P813','P31']
+        self.data = ['P854', 'P813', 'P31', 'P248']
 
     def test_check_reference_specificity_from_Wikdiata(self):
-        ret = RefPropertiesConsistencyChecker(self.data)
-        result=ret.check_reference_specificity_from_Wikdiata()
-        self.assertEqual(result[0].is_ref_specific, True)
-        self.assertEqual(result[1].is_ref_specific, True)
-        self.assertEqual(result[2].is_ref_specific, False)
-
-
-
-        
-
+        test_class = RefPropertiesConsistencyChecker(self.data)
+        result = test_class.check_reference_specificity_from_Wikdiata()
+        self.assertGreaterEqual(test_class.score, 0)
+        self.assertLessEqual(test_class.score, 1)
+        with open('ref_consistency_ratio.test.csv', 'w') as file_handler:
+            file_handler.write(str(test_class))
+        with open('ref_consistency.test.csv', 'w', newline='') as f:
+            w = csv.writer(f)
+            # write header from NamedTuple fields
+            w.writerow([field for field in test_class.results[0]._fields])
+            for result in test_class.results:
+                row = [result._asdict()[field] for field in result._fields]
+                w.writerow(row)
