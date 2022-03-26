e576242c10f32c1591788c8d4f8d99104e3b70a2
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-03-26 23:14:33 +0000

    added code

--- RQSSFramework/Completeness/SchemaBasedRefPropertiesCompletenessChecking.py
+++ RQSSFramework/Completeness/SchemaBasedRefPropertiesCompletenessChecking.py
@@ -1,14 +1,15 @@
 from typing import Dict, List, NamedTuple
 
-from EntitySchemaExtractor import EidRefSummary
+from EntitySchemaExtractor import EidRefSummary, RefedFactRef
 
 
 class SchemaBasedCompletenessResult(NamedTuple):
     class_id: str
     fact_predicate_id: str
     ref_predicate_id: str
-    total_instances:int = 0
-    total_refed_instances: int = 0      # total number of instances with the ref_predicate_id
+    total_instances: int = 0
+    # total number of instances with the ref_predicate_id
+    total_refed_instances: int = 0
 
     def __repr__(self):
         return "Class {0}, with fact predicate: {1}, has a reference predicate: {2} in the schema level (E-ids); total instances: {3}; total referenced instances: {4} ".format(self.class_id, self.fact_predicate_id, self.ref_predicate_id, self.total_instances, self.total_refed_instances)
@@ -16,13 +17,14 @@ class SchemaBasedCompletenessResult(NamedTuple):
 
 class SchemaBasedRefPropertiesCompletenessChecker:
     results: List[SchemaBasedCompletenessResult] = None
+    _refed_facts: List[RefedFactRef]
 
-    def __init__(self):
-        pass
-
+    def __init__(self, dataset_refed_facts: List[RefedFactRef]):
+        self._refed_facts = dataset_refed_facts
+    
     def check_schema_based_property_completeness_Wikidata(self, wikidata_entityschemas_ref_summery: List[EidRefSummary]) -> List[SchemaBasedCompletenessResult]:
         self.results = []
-        return self.results    
+        return self.results
 
     def __repr__(self):
         if self.results == None:
