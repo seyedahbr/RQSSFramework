944a01e0dd04b8c80a2463ddb441827cb9cffee4
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-06-04 22:41:23 +0100

    Completed schema based comleteness code

--- RQSSFramework/Completeness/SchemaBasedRefPropertiesCompletenessChecking.py
+++ RQSSFramework/Completeness/SchemaBasedRefPropertiesCompletenessChecking.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
-from typing import List, NamedTuple
+from typing import Dict, List, NamedTuple
 
+import numpy as np
 import pandas as pd
-from RQSSFramework.EntitySchemaExtractor import EidRefSummary
+from EntitySchemaExtractor import EidRefSummary
 
 
 class SchemaBasedCompletenessResult(NamedTuple):
@@ -10,16 +11,16 @@ class SchemaBasedCompletenessResult(NamedTuple):
     ref_predicate: str
     total_instances: int
     total_instances_not_refed: int
-    total_refed_instances: int
+    total_refed_instances_schema_based: int
 
     @property
     def score(self):
-        return self.total_refed_instances / self.total_instances if self.total_instances > 0 else 1
+        return self.total_refed_instances_schema_based / self.total_instances if self.total_instances > 0 else 1
 
     @property
     def score_including_not_refed(self):
         including_not_refed = self.total_instances + self.total_instances_not_refed
-        return self.total_refed_instances / including_not_refed if including_not_refed > 0 else 1
+        return self.total_refed_instances_schema_based / including_not_refed if including_not_refed > 0 else 1
 
     def __repr__(self):
         return '''For fact {0} and reference properties {1} in the schema-level,\n
@@ -27,7 +28,7 @@ class SchemaBasedCompletenessResult(NamedTuple):
 \tTotal not referenced fact instances: {3}
 \tTotal referenced fact (with {1}) instances: {4}
 \tScore: {5}
-\tScore (including not referenced instances): {6}'''.format(self.fact, self.ref_predicate, self.total_instances, self.total_instances_not_refed, self.total_refed_instances, self.score, self.score_including_not_refed)
+\tScore (including not referenced instances): {6}'''.format(self.fact, self.ref_predicate, self.total_instances, self.total_instances_not_refed, self.total_refed_instances_schema_based, self.score, self.score_including_not_refed)
 
 
 @dataclass
@@ -36,63 +37,83 @@ class FactRef:
     fact: str
     ref_predicate: str = None
 
-    # def __repr__(self) -> str:
-    #     return "Fact: {1}, Reference Property: {2}".format(self.fact, self.ref_predicate)
+    def __repr__(self) -> str:
+        return "Fact: {1}, Reference Property: {2}".format(self.fact, self.ref_predicate)
 
 
 class SchemaBasedRefPropertiesCompletenessChecker:
     results: List[SchemaBasedCompletenessResult] = None
-    _input: List[FactRef]
+    _input: List[FactRef]  # it is equivalent to the set "H" in the manuscript
 
     def __init__(self, dataset: List[FactRef]):
         self._input = dataset
+        self._df = pd.DataFrame(
+            columns=['statement_id', 'fact', 'ref_predicate'])
+        for x in self._input:
+            self._df = self._df.append({
+                'statement_id': x.statement_id,
+                'fact': x.fact,
+                'ref_predicate': x.ref_predicate if x.ref_predicate is not None else np.nan}, ignore_index=True)
 
     def check_schema_based_property_completeness_Wikidata(self, wikidata_entityschemas_ref_summery: List[EidRefSummary]) -> List[SchemaBasedCompletenessResult]:
         self.results = []
-        df = pd.DataFrame(self._input, columns=['statement_id', 'fact', 'ref_predicate'])
-        print(df)
-        for schema in wikidata_entityschemas_ref_summery:
-            for fact_ref in schema.refed_facts_refs:
-                for ref in fact_ref.ref_predicates:
-                    total_instances = len(
-                        [x for x in self._input if x.fact == fact_ref.refed_fact and x.ref_predicate is not None])
-                    total_instances_not_refed = len(
-                        [x for x in self._input if x.fact == fact_ref.refed_fact])
-                    total_refed_instances = len(
-                        [x for x in self._input if x.fact == fact_ref.refed_fact and x.ref_predicate is not None and x.ref_predicate == ref])
-                    self.results.append(SchemaBasedCompletenessResult(
-                        fact_ref.refed_fact, ref, total_instances, total_instances_not_refed, total_refed_instances))
+        eschema_facts_refs = self._get_eschema_distinct_properties_and_ref_predicates(
+            wikidata_entityschemas_ref_summery)
+
+        for fact in eschema_facts_refs.keys():
+            for ref in eschema_facts_refs[fact]:
+                total_instances = len(self._df.loc[(self._df['fact'] == fact) & ~(
+                    self._df['ref_predicate'].isna())].drop_duplicates('statement_id'))
+                total_instances_not_refed = len(self._df.loc[(
+                    self._df['fact'] == fact)].drop_duplicates('statement_id'))
+                total_refed_instances_schema_based = len(self._df.loc[(self._df['fact'] == fact) & (
+                    self._df['ref_predicate'] == ref)].drop_duplicates('statement_id'))
+                self.results.append(SchemaBasedCompletenessResult(
+                    fact, ref, total_instances, total_instances_not_refed, total_refed_instances_schema_based))
 
         return self.results
+
+    def _get_eschema_distinct_properties_and_ref_predicates(self, wikidata_entityschemas_ref_summery: List[EidRefSummary]) -> Dict:
+        ret_dict = {}
+        for schema in wikidata_entityschemas_ref_summery:
+            for fact_ref in schema.refed_facts_refs:
+                if fact_ref.refed_fact not in ret_dict.keys():
+                    ret_dict[str(fact_ref.refed_fact)] = []
+                for ref in fact_ref.ref_predicates:
+                    if ref not in ret_dict[str(fact_ref.refed_fact)]:
+                        ret_dict[str(fact_ref.refed_fact)].append(ref)
+        return ret_dict
 
     @property
     def score(self):
         if self.results != None:
-            total = sum([i.total_instances for i in self.results])
-            return sum([i.total_refed_instances for i in self.results])/total if total > 0 else 1
+            total = len(
+                [i for i in self._input if i.ref_predicate is not None])
+            return sum([i.score for i in self.results])/total if total > 0 else 1
         return None
 
     @property
     def score_including_not_refed(self):
         if self.results != None:
-            total_including_not_refed = sum(
-                [(i.total_instances + i.total_instances_not_refed) for i in self.results])
-            return sum([i.total_refed_instances for i in self.results])/total_including_not_refed if total_including_not_refed > 0 else 1
+            total_including_not_refed = len(self._input)
+            return sum([i.score_including_not_refed for i in self.results])/total_including_not_refed if total_including_not_refed > 0 else 1
         return None
 
     def __repr__(self):
         if self.results == None:
             return 'Results are not computed'
         return """total fact-reference pairs,total facts without reference,total facts (distinct),total referenced facts (distinct),total reference properties (distinct),total reference-specific predicates mentioned in schema level (distinct),total facts that are referenced with a reference-specific property mentioned in the schema level,score,score including not refed
-{0},{1},{2},{3},{4},{5},{6},{7},{8}""".format(len([i for i in self._input if i.ref_predicate is not None]),
-                                  len([i for i in self._input if i.ref_predicate is None]),
-                                  len(list(dict.fromkeys([i.fact for i in self._input]))),
-                                  len(list(dict.fromkeys([i.fact for i in self._input if i.ref_predicate is not None]))),
-                                  len(list(dict.fromkeys([i.ref_predicate for i in self._input]))),
-                                  len(list(dict.fromkeys([i.ref_predicate for i in self.results]))),
-                                  sum([i.total_refed_instances for i in self.results]),
-                                  self.score,
-                                  self.score_including_not_refed)
+{0},{1},{2},{3},{4},{5},{6},{7},{8}""".format(
+            len([i for i in self._input if i.ref_predicate is not None]),
+            len([i for i in self._input if i.ref_predicate is None]),
+            len(list(dict.fromkeys([i.fact for i in self._input]))),
+            len(list(dict.fromkeys(
+                [i.fact for i in self._input if i.ref_predicate is not None]))),
+            len(list(dict.fromkeys([i.ref_predicate for i in self._input]))),
+            len(list(dict.fromkeys([i.ref_predicate for i in self.results]))),
+            sum([i.total_refed_instances_schema_based for i in self.results]),
+            self.score,
+            self.score_including_not_refed)
 
     def print_results(self):
         """
