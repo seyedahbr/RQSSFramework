13d7ee5e2cb99162d3e3e66e09525d0576850ec5
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-06-10 23:47:31 +0100

    Worked on property completeness class

--- RQSSFramework/Completeness/PropertyCompletenessChecking.py
+++ RQSSFramework/Completeness/PropertyCompletenessChecking.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List, NamedTuple
+import pandas as pd
 
 from EntitySchemaExtractor import EidRefSummary
 from SchemaBasedRefPropertiesCompletenessChecking import FactRef
@@ -33,21 +34,51 @@ class PropertyCompletenessResult(NamedTuple):
 
 class PropertyCompletenessChecker:
     results: List[PropertyCompletenessResult] = None
-    _facts_refs: List[FactRef]
+    _input: List[FactRef]
 
     def __init__(self, dataset_facts_refs: List[FactRef]):
-        self._facts_refs = dataset_facts_refs
+        self._input = dataset_facts_refs
+        self._df = pd.DataFrame(
+            columns=['statement_id', 'fact', 'ref_predicate'])
+        for x in self._input:
+            self._df = self._df.append({
+                'statement_id': x.statement_id,
+                'fact': x.fact,
+                'ref_predicate': x.ref_predicate if x.ref_predicate is not None else np.nan}, ignore_index=True)
 
     def check_property_completeness_Wikidata(self) -> List[PropertyCompletenessResult]:
         self.results = []
         return self.results
+        
 
     @property
     def score(self):
-        pass
+        if self.results != None:
+            total = len(
+                [i for i in self._input if i.ref_predicate is not None])
+            return sum([i.score for i in self.results])/total if total > 0 else 1
+        return None
+
+    @property
+    def score_including_not_refed(self):
+        if self.results != None:
+            total_including_not_refed = len(self._input)
+            return sum([i.score_including_not_refed for i in self.results])/total_including_not_refed if total_including_not_refed > 0 else 1
+        return None
 
     def __repr__(self):
-        pass
+        if self.results == None:
+            return 'Results are not computed'
+        return """total fact-reference pairs,total facts without reference,total facts (distinct),total referenced facts (distinct),total reference properties (distinct),score,score including not refed
+{0},{1},{2},{3},{4},{5},{6},{7},{8}""".format(
+            len([i for i in self._input if i.ref_predicate is not None]),
+            len([i for i in self._input if i.ref_predicate is None]),
+            len(list(dict.fromkeys([i.fact for i in self._input]))),
+            len(list(dict.fromkeys(
+                [i.fact for i in self._input if i.ref_predicate is not None]))),
+            len(list(dict.fromkeys([i.ref_predicate for i in self._input]))),
+            self.score,
+            self.score_including_not_refed)
 
     def print_results(self):
         """
