9e198401214c4b2b5470881bf7edceec165f358d
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-06-11 19:35:20 +0100

    Completed writing property completeness checker

--- RQSSFramework/Completeness/PropertyCompletenessChecking.py
+++ RQSSFramework/Completeness/PropertyCompletenessChecking.py
@@ -1,8 +1,7 @@
-from dataclasses import dataclass
-from typing import List, NamedTuple
+from typing import Dict, List, NamedTuple
+
 import pandas as pd
 
-from EntitySchemaExtractor import EidRefSummary
 from SchemaBasedRefPropertiesCompletenessChecking import FactRef
 
 
@@ -31,20 +30,45 @@ class PropertyCompletenessResult(NamedTuple):
 \tScore (including not referenced instances): {6}'''.format(self.ref_predicate, self.fact, self.total_instances, self.total_instances_not_refed, self.total_refed, self.score, self.score_including_not_refed)
 
 
-
 class PropertyCompletenessChecker:
     results: List[PropertyCompletenessResult] = None
     _input: List[FactRef]
 
     def __init__(self, dataset_facts_refs: List[FactRef]):
         self._input = dataset_facts_refs
-        if not self._input: pass
-        self._df = pd.DataFrame.from_records(self._input, columns=self._input[0]._fields)
+        if not self._input:
+            pass
+        self._df = pd.DataFrame.from_records(
+            self._input, columns=self._input[0]._fields)
 
     def check_property_completeness_Wikidata(self) -> List[PropertyCompletenessResult]:
         self.results = []
+        fact_equivalence_classes_refs = self._get_distinct_fact_and_ref_predicates()
+        print('Fact equivalence classes and refs:\n',
+              fact_equivalence_classes_refs)
+        for fact in fact_equivalence_classes_refs:
+            for ref in fact_equivalence_classes_refs[fact]:
+                print('computing fact {0}, ref {1}'.format(fact, ref))
+                total_instances = len(self._df.loc[(self._df['fact'] == fact) & ~(
+                    self._df['ref_predicate'].isna())].drop_duplicates('statement_id'))
+                total_instances_not_refed = len(self._df.loc[(
+                    self._df['fact'] == fact)].drop_duplicates('statement_id'))
+                total_refed = len(self._df.loc[(self._df['fact'] == fact) & (
+                    self._df['ref_predicate'] == ref)].drop_duplicates('statement_id'))
+                self.results.append(PropertyCompletenessResult(
+                    fact, ref, total_instances, total_instances_not_refed, total_refed))
         return self.results
-        
+
+    def _get_distinct_fact_and_ref_predicates(self) -> Dict:
+        ret_dict = {}
+        for fact_ref in self._input:
+            if fact_ref.ref_predicate is None:
+                continue
+            if fact_ref.fact not in ret_dict.keys():
+                ret_dict[fact_ref.fact] = []
+            if fact_ref.ref_predicate not in ret_dict[fact_ref.fact]:
+                ret_dict[fact_ref.fact].append(fact_ref.ref_predicate)
+        return ret_dict
 
     @property
     def score(self):
@@ -65,7 +89,7 @@ class PropertyCompletenessChecker:
         if self.results == None:
             return 'Results are not computed'
         return """total fact-reference pairs,total facts without reference,total facts (distinct),total referenced facts (distinct),total reference properties (distinct),score,score including not refed
-{0},{1},{2},{3},{4},{5},{6},{7},{8}""".format(
+{0},{1},{2},{3},{4},{5},{6}""".format(
             len([i for i in self._input if i.ref_predicate is not None]),
             len([i for i in self._input if i.ref_predicate is None]),
             len(list(dict.fromkeys([i.fact for i in self._input]))),
