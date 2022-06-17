8c13e021594b4560ad184b09a7d89e7ae3c35e8e
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-06-17 19:39:55 +0100

    Modified range consistency tester

--- tests/test_TestTriplesRangeConsistencyChecker.py
+++ tests/test_TestTriplesRangeConsistencyChecker.py
@@ -8,10 +8,15 @@ from RQSSFramework.Consistency.TriplesRangeConsistencyChecking import \
 class TestTripleRangeConsistencyChecking(unittest.TestCase):
 
     def setUp(self):
-        self.data = {'P854': ['Q1', 'Q2'], 'P813': [
-            'Q3'], 'P248': ['Q4', 'Q5', 'Q6']}
+        self.data = {'P854': ['Q1', 'Q2', 'Q1', 'Q2', 'Q3', 'Q2'], 'P813': [
+            'Q3', 'Q1','Q3', 'Q1','Q3', 'Q1'], 'P248': ['Q4', 'Q5', 'Q6']}
 
     def test_get_property_regex_from_Wikidata(self):
+        ret = TriplesRangeConsistencyChecker(
+            self.data).get_property_ranges_from_Wikidata()
+        self.assertEqual(ret.keys(), self.data.keys())
+    
+    def test_get_instances_subclass_of_values_from_Wikidata(self):
         ret = TriplesRangeConsistencyChecker(
             self.data).get_property_ranges_from_Wikidata()
         self.assertEqual(ret.keys(), self.data.keys())
