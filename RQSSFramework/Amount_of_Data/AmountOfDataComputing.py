32cafe20b77a7f00766518e05995b5bc96c6dad4
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-07-06 23:18:40 +0100

    Added code to amount-of-data

--- RQSSFramework/Amount_of_Data/AmountOfDataComputing.py
+++ RQSSFramework/Amount_of_Data/AmountOfDataComputing.py
@@ -1,7 +1,38 @@
 from typing import List, NamedTuple
 
+
+class RefNodeOutgoing(NamedTuple):
+    ref_node: str
+    num_of_out: int
+
+
 class AmountOfDataComputer:
-    def __init__(self):
+    _triple_dist: List[RefNodeOutgoing]
+    _literal_dist: List[RefNodeOutgoing]
+    _num_of_statement_node: int
+    _num_of_ref_node: int
+
+    def __init__(self, triple_dist: List[RefNodeOutgoing], literal_dist: List[RefNodeOutgoing], num_of_statement_node: int, num_of_ref_node: int):
+        self._triple_dist = triple_dist
+        self._literal_dist = literal_dist
+        self._num_of_statement_node = num_of_statement_node
+        self._num_of_ref_node = num_of_ref_node
+
+    @property
+    def ref_node_per_statement_ratio(self):
         pass
+
+    @property
+    def ref_triple_per_statement_ratio(self):
+        pass
+
+    @property
+    def ref_triple_per_ref_node_ratio(self):
+        pass
+
+    @property
+    def ref_literal_per_ref_node_ratio(self):
+        pass
+
     def __repr__(self):
         pass
