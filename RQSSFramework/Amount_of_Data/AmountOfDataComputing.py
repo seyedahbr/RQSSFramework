4f1d4085bd370e02b75ea71cda7cf07aea017308
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-07-18 01:50:49 +0100

    Modified the computation of literal ratio

--- RQSSFramework/Amount_of_Data/AmountOfDataComputing.py
+++ RQSSFramework/Amount_of_Data/AmountOfDataComputing.py
@@ -33,14 +33,15 @@ class AmountOfDataComputer:
         return num_ref_triple / self._num_ref_node
 
     @property
-    def ref_literal_per_ref_node_ratio(self):
+    def ref_literal_per_ref_triple_ratio(self):
+        num_ref_triple = sum([i.num_out for i in self._triple_dist])
         num_ref_literal = sum([i.num_out for i in self._literal_dist])
-        return num_ref_literal / self._num_ref_node
+        return num_ref_literal / num_ref_triple
 
     def __repr__(self):
-        return """ref node per statement ratio,ref triple per statement ratio,ref triple per ref node ratio,ref literal per ref node ratio
+        return """ref node per statement ratio,ref triple per statement ratio,ref triple per ref node ratio,ref literal per ref triple ratio
 {0},{1},{2},{3}""".format(
             self.ref_node_per_statement_ratio,
             self.ref_triple_per_statement_ratio,
             self.ref_triple_per_ref_node_ratio,
-            self.ref_literal_per_ref_node_ratio)
+            self.ref_literal_per_ref_triple_ratio)
