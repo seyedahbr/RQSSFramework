ee173e70443c3c001ee3315ffd201f13fee34e8b
Seyed Amir Hosseini Beghaeiraveri <a.beghaei@gmail.com>
2022-08-02 23:02:30 +0100

    Added external sources query

--- RQSSFramework/Queries.py
+++ RQSSFramework/Queries.py
@@ -10,6 +10,26 @@ SELECT ?to_ret WHERE
 
 Limit 3
 ''',
+"get_all_external_sources_distinct" : 
+'''
+PREFIX prov: <http://www.w3.org/ns/prov#>
+SELECT DISTINCT ?to_ret WHERE {
+   ?statement prov:wasDerivedFrom ?ref .
+   ?ref ?refProperty ?to_ret .
+  FILTER (isIRI(?to_ret) &&
+          !CONTAINS(lcase(str(?to_ret)), "wikipedia.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikimedia.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikivoyage.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "mediawiki.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikiversity.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "wikinews.org")  &&
+          !CONTAINS(lcase(str(?to_ret)), "wikisource.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "wikibooks.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wikiquote.org") &&
+          !CONTAINS(lcase(str(?to_ret)), "wiktionary.org")&&
+          !CONTAINS(lcase(str(?to_ret)), "wikiba.se"))
+}
+''',
 "get_property_equivalents_Wikidata":
 '''
 SELECT (COUNT (?propEq) AS ?to_ret)
